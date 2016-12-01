4b7455440294e13242455cbec3c7c7ca846a1555
RememberMeWell <thejbguitaryt@gmail.com>
2016-12-01 16:25:21 +0100

    added database

---
+++ src/HoneyMoonShop/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoneymoonShop.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public int Phonenumber { get; set; }
+    }
+}
