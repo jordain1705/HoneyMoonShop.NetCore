8ecc97b0373389d1e1bf5e04d7827e2a5c500186
jordain1705 <JordainGijsbertha@JORDAINGIJS260F>
2016-12-15 19:04:49 +0100

    Dressfinder
    Dit is de dressfinder met een werkende popout en een begin aan de filter
    lijst. Het heeft ook nieuwe header footer en menu.

--- src/HoneyMoonShop/Controllers/HomeController.cs
+++ src/HoneyMoonShop/Controllers/HomeController.cs
@@ -15,7 +15,46 @@ namespace HoneymoonShop.Controllers
 
         public IActionResult Dressfinder()
         {
-            return View();
+            List<string> merken = new List<string>();  //hier komt later een query die de lijst vult
+            merken.Add("dolce");
+            merken.Add("gucci");
+            merken.Add("zeeman");
+            ViewData["merken"] = merken;
+
+            List<string> stijlen = new List<string>();  //hier komt later een query die de lijst vult
+            merken.Add("cool");
+            merken.Add("stoer");
+            merken.Add("mooi");
+            ViewData["stijlen"] = stijlen;
+
+            List<double> prijzen = new List<double>();  //hier komt later een query die de lijst vult
+            prijzen.Add(1.99);
+            prijzen.Add(20.00);
+            prijzen.Add(1290.32);
+            ViewData["minprijs"] = (int)prijzen.Min();
+            ViewData["maxprijs"] = (int)prijzen.Max();
+
+            List<string> neklijnen = new List<String>(); //hier komt later een query die de lijst vult
+            neklijnen.Add("V-hals");
+            neklijnen.Add("kraag");
+            neklijnen.Add("zwarte pieten band");
+            ViewData["neklijnen"] = neklijnen;
+
+            List<string> silhouette = new List<String>(); //hier komt later een query die de lijst vult
+            silhouette.Add("peer");
+            silhouette.Add("zandloper");
+            silhouette.Add("appel");
+            ViewData["silhouette"] = silhouette;
+
+            List<string> kleuren = new List<String>(); //hier komt later een query die de lijst vult
+            kleuren.Add("ivoor/wit");
+            kleuren.Add("ivoor met kleur");
+            kleuren.Add("gekleurd");
+            ViewData["kleuren"] = kleuren;
+
+
+
+            return View(merken);
         }
 
         public IActionResult About()
