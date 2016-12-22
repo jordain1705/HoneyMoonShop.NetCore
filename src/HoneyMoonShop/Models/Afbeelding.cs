1ce0dd3dd2d7b62026631b3cb84cdff9471864e4
Fleur <fleurarkesteijn@gmail.com>
2016-12-22 12:09:24 +0100

    sync

--- src/HoneyMoonShop/Models/Afbeelding.cs
+++ src/HoneyMoonShop/Models/Afbeelding.cs
@@ -8,7 +8,7 @@ namespace HoneymoonShop.Models
     public class Afbeelding
     {
         public String AfbeeldingId { get; set; }
-        public String Artikelnummer { get; set; }
+        public int Artikelnummer { get; set; }
         public String SourcePath { get; set; }
 
         public Jurk Jurk { get; set; }
