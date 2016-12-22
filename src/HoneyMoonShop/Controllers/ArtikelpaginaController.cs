b17e8b690f14b0985ec06436f8c49f09e5dd3e98
Fleur <fleurarkesteijn@gmail.com>
2016-12-22 15:20:27 +0100

    mergie

--- src/HoneyMoonShop/Controllers/ArtikelpaginaController.cs
+++ src/HoneyMoonShop/Controllers/ArtikelpaginaController.cs
@@ -11,19 +11,38 @@ namespace HoneymoonShop.Controllers
 {
     public class ArtikelpaginaController : Controller
     {
-      
-        public IActionResult Artikelpagina() {
-          
+        public int artikelnummer { get; private set; }
+
+  
+        public IActionResult Artikelpagina(int artikelnummer) {
+
+            
             using (var context = new HoneyMoonShopContext()) {
-                List<string> plaatjesjurk = new List<string>();
-                var plaatjejurk = Afbeelding.Select(s => new { Afbeelding = s.Afbeelding }); 
-                var selectArtikel = Jurk.Select(s => new {
+               //var  anummer = context.Jurken.Select(s => s.Artikelnummer);
+                //string arti = anummer.ToString();
+                //int artikelnummer= int.Parse(arti);
+                
+                List<String> plaatjesjurk = context.Afbeeldingen.Where(s => s.Artikelnummer==(artikelnummer)).Select(s =>  s.SourcePath ).ToList();
+                ViewData["plaatjes"] = plaatjesjurk;
+
+               String merken = context.Jurken.Where(s => s.Artikelnummer == artikelnummer).Select(s => s.Merk).ToString();
+                ViewData["merkje"] = merken;
+
+                List<String> stijlk = context.Jurken.Where(s => s.Artikelnummer == artikelnummer).Select(s => s.Stijl).ToList();
+                ViewData["merkje"] = merken;
+                //List<string> hooiwagenplaatjesjurk = new List<string>();
+                // foreach(var a in context.Afbeeldingen) { 
+                //   var plaatjejurk = context.Afbeeldingen.Where(s => s.Artikelnummer == artikelnummer).Select(s => new { Afbeelding = s.SourcePath }); 
+
+
+                var selectArtikel =
+                    context.Jurken.Select(s => new {
                     Naam = s.Merk,
                     nummer = s.Artikelnummer,
-                    omshrijving = s.omshrijving,
+                    omshrijving = s.Omschrijving,
                     Merk = s.Merk,
-                    Stijl = s.Materiaal,
-                    prijs = s.prijs
+                    Stijl = s.Stijl,
+                    Prijsindicatie= s.MaxPrijs
                 });
 
             }
