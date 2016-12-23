c3a9ef4339a717a85e6ae80ce962cae0a8dea26a
Fleur <fleurarkesteijn@gmail.com>
2016-12-23 13:40:43 +0100

    Artikelpagina

--- src/HoneyMoonShop/Controllers/ArtikelpaginaController.cs
+++ src/HoneyMoonShop/Controllers/ArtikelpaginaController.cs
@@ -19,6 +19,8 @@ namespace HoneymoonShop.Controllers
                 //var  anummer = context.Jurken.Select(s => s.Artikelnummer);
                 //string arti = anummer.ToString();
                 //int artikelnummer= int.Parse(arti);
+              
+                ViewData["curJurk"]= curJurk;
 
                 int a = curJurk.Artikelnummer;
                 ViewData["nummer"] = a;
@@ -27,32 +29,43 @@ namespace HoneymoonShop.Controllers
                 ViewData["maxi"] = maxp;
 
                 int minp = curJurk.MinPrijs; 
-                
                 ViewData["min"] = minp;
 
-                String teks = curJurk.Omschrijving;
+                string teks = curJurk.Omschrijving;
                 ViewData["Omschrijving"] = teks;
 
                 String merken = curJurk.Merk;
                 ViewData["merkje"] = merken;
 
+                String stylo = curJurk.Stijl;
+                ViewData["stylo"] = stylo;
+
+                String nl = curJurk.Neklijn;
+                ViewData["nek"] = nl;
+
+                String sil = curJurk.Silhouette;
+                ViewData["silh"] = sil;
+
+              
                 List<String> stijlk = context.Jurken.Where(s => s.Artikelnummer == curJurk.Artikelnummer).Select(s => s.Stijl).ToList();
                 ViewData["merkje"] = merken;
-                //List<string> hooiwagenplaatjesjurk = new List<string>();
-                // foreach(var a in context.Afbeeldingen) { 
-                //   var plaatjejurk = context.Afbeeldingen.Where(s => s.Artikelnummer == artikelnummer).Select(s => new { Afbeelding = s.SourcePath }); 
 
-            
-                var selectArtikel =
-                    context.Jurken.Select(s => new {
-                    Naam = s.Merk,
-                    nummer = s.Artikelnummer,
-                    omshrijving = s.Omschrijving,
-                    Merk = s.Merk,
-                    Stijl = s.Stijl,
-                    Prijsindicatie= s.MaxPrijs
-                });
+                List<string> plaatjesjurk = new List<string>();
+               
 
+                string img1 = "/Images/" + a + "a.png";
+                string img2 = "/Images/" + a +"b.png";
+                string img3 = "/Images/" + a+ "c.png";
+                
+                ViewData["pl1"] = img1;
+                
+                ViewData["pl2"] = img2;
+               
+                ViewData["pl3"] = img3;
+                
+               
+
+                
             }
             return View();
         }
