e1f65bcdabd040fd8a907f7292dc57debf0baec1
RememberMeWell <booij.jiri@gmail.com>
2017-01-09 13:58:48 +0100

    ?

---
+++ src/HoneyMoonShop/Controllers/DressFinderController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HoneymoonShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoneymoonShop.Controllers
+{
+    public class DressFinderController : Controller
+    {
+        public IActionResult Dressfinder()
+        {
+
+            using (var context = new HoneyMoonShopContext())
+            {
+                // .Where(b => b.Artikelnummer.Equals(12649));
+                List<string> alleMerken = context.Jurken.Select(g => g.Merk).Distinct().ToList();
+                ViewData["merken"] = alleMerken;
+
+                List<string> alleStijlen = context.Jurken.Select(g => g.Stijl).Distinct().ToList();
+                ViewData["stijlen"] = alleStijlen;
+
+                List<int> minprijs = context.Jurken.Select(g => g.MinPrijs).ToList();
+                ViewData["minprijs"] = minprijs.Min();
+
+                List<int> maxprijs = context.Jurken.Select(g => g.MaxPrijs).ToList();
+                ViewData["maxprijs"] = maxprijs.Max();
+
+                List<string> neklijnen = context.Jurken.Select(g => g.Neklijn).Distinct().ToList();
+                ViewData["neklijnen"] = neklijnen;
+
+                List<string> silhouettes = context.Jurken.Select(g => g.Silhouette).Distinct().ToList();
+                ViewData["silhouette"] = silhouettes;
+
+                 List<Jurk> jurken = context.Jurken.ToList();
+                ViewData["jurken"] = jurken;
+
+                List<string> kleuren = new List<String>(); //todo: hier komt later een query die de lijst vult
+                kleuren.Add("ivoor/wit");
+                kleuren.Add("ivoor met kleur");
+                kleuren.Add("gekleurd");
+                ViewData["kleuren"] = kleuren;
+
+                return View(jurken);
+            }
+        }
+
+        public IActionResult FilterVerwerken()
+        {
+            return PartialView("ProductsPartial");
+        }
+    }
+}
+
---
+++ src/HoneyMoonShop/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using HoneymoonShop.Models;
+
+namespace HoneymoonShop
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            String ConnectionString =
+                @"Data Source=honeymoonshop.database.windows.net;Initial Catalog=HoneyMoonShop;Integrated Security=False;User ID=Project6;Password=Honeymoon1;Connect Timeout=15;";
+            services.AddDbContext<HoneyMoonShopContext>(options => options.UseSqlServer(ConnectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //call the seed method in the DbContextExtensions class
+          //app.ApplicationServices.GetRequiredService<HoneyMoonShopContext>().Seed(); 
+        }
+    }
+}
