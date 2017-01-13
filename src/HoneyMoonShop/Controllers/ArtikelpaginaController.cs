75f039087c6068d13694ad45f486967506b37e8f
RememberMeWell <booij.jiri@gmail.com>
2017-01-13 10:52:53 +0100

    added fleurs code and 'alle jurken bekijken' knop werkt nu

--- src/HoneyMoonShop/Controllers/ArtikelpaginaController.cs
+++ src/HoneyMoonShop/Controllers/ArtikelpaginaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using HoneymoonShop.Models;
 using System.Linq;
 using System.Collections.Generic;
+using HoneymoonShop.Data;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
